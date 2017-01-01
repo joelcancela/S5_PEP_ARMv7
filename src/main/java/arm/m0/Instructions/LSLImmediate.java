376e3d4929176b53fb30fd889a4fb4c15c20f3fe
Nikita Rousseau <warhawk3407@gmail.com>
2017-01-01 21:17:28 +0100

    wip lsl 2, patched warning

--- src/main/java/arm/m0/Instructions/LSLImmediate.java
+++ src/main/java/arm/m0/Instructions/LSLImmediate.java
@@ -3,6 +3,8 @@ package arm.m0.Instructions;
 /**
  * Created by Joel CANCELA VAZ on 01/01/2017.
  */
+
+// A7-298
 public class LSLImmediate {
 	String imm5;
 	String rm;
@@ -11,13 +13,13 @@ public class LSLImmediate {
 	public LSLImmediate(String[] args){
 
 		//args[2] = nb entre 0-31 on veut le binaire sur 5 bits
-		imm5 = String.format("%05d", Integer.toBinaryString(Integer.parseInt(args[2])));
+		imm5 = String.format("%05d", Integer.parseInt(Integer.toBinaryString(Integer.parseInt(args[2]))));
 
 		//args[1] = "Rx" x un nombre on veut son binaire sur 3 bits
-		rm = String.format("%03d", Integer.toBinaryString(Integer.parseInt(args[1])));
+		rm = String.format("%03d", Integer.parseInt(Integer.toBinaryString(Integer.parseInt(args[1].substring(1)))));
 
 		//args[0] = "Rx" x un nombre on veut son binaire sur 3 bits
-		rd = String.format("%03d", Integer.toBinaryString(Integer.parseInt(args[0])));
+		rd = String.format("%03d", Integer.parseInt(Integer.toBinaryString(Integer.parseInt(args[0].substring(1)))));
 	}
 
 	@Override
